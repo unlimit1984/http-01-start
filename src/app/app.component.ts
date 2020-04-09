3459ceed42aa3047bd6beb56bf2e19057e689a8b
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-04-09 23:05:13 +0200

    [Http] 256 Use pipe and map and spread-operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -39,6 +40,15 @@ export class AppComponent implements OnInit {
 
   private fetchPosts() {
     this.http.get('https://ng-complete-guide-ef715.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
+        const postArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postArray.push({...responseData[key], id: key});
+          }
+        }
+        return postArray;
+      }))
       .subscribe(posts => {
         console.log(posts);
       });
