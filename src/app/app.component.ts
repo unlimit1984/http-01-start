7d15ad5546e74f7e48cf33ec0a248df80feb59a3
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-04-12 18:39:42 +0200

    [Http] 263 Add Error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { PostsService } from './posts.service';
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
+  error = null;
 
   constructor(private postService: PostsService) {
   }
@@ -20,6 +21,9 @@ export class AppComponent implements OnInit {
       .subscribe(posts => {
         this.isFetching = false;
         this.loadedPosts = posts;
+      }, error => {
+        this.error = error.message;
+        console.log(error);
       });
   }
 
@@ -36,6 +40,8 @@ export class AppComponent implements OnInit {
       .subscribe(posts => {
         this.isFetching = false;
         this.loadedPosts = posts;
+      }, error => {
+        this.error = error.message;
       });
   }
 
