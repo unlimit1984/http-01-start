f47b1e74c622ef750b8de3df08fba7ba1c78854f
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-04-12 18:55:07 +0200

    [Http] 264 Use Subject for Error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Post } from './post.model';
 import { PostsService } from './posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
+  private errorSub: Subscription;
 
   constructor(private postService: PostsService) {
   }
 
   ngOnInit() {
+
+    this.errorSub = this.postService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    });
+
     this.isFetching = true;
     this.postService.fetchPosts()
       .subscribe(posts => {
@@ -51,5 +58,9 @@ export class AppComponent implements OnInit {
       .subscribe(() => {
         this.loadedPosts = [];
       });
+  }
+
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
   }
 }
